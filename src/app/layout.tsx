398b19d2faf19bcded4c08e35d05ac39fd8beebb
Ty Ha <tyha@Host-003.lan>
2025-06-27 12:30:23 +0200

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,8 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-title" content="DecodEmojis" />
         <meta name="mobile-web-app-capable" content="yes" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>�️</text></svg>" />
+        <link rel="shortcut icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>�️</text></svg>" />
       </head>
       <body className={inter.className + ' !bg-white'}>
         <div className="flex flex-col min-h-screen pb-16 md:pb-0">
